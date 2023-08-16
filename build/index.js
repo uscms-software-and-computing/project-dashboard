6de391a970199b9712169114990f418fa48c2d4f
Rob Tuck <rctuck@princeton.edu>
2023-08-16 17:41:33 -0400

    Adding build

---
+++ build/index.js
@@ -0,0 +1,22 @@
+"use strict";
+async function getWorkPackages() {
+    const response = await fetch("https://uscms-software-and-computing.github.io/OpenProject_data/data/all_results.json");
+    const workPackages = await response.json();
+    let nameAndDate;
+    nameAndDate = workPackages["_embedded"].elements.map((x) => ({
+        subject: x.subject,
+        progress: x.percentageDone
+    }));
+    // console.log(nameAndDate);
+    return (nameAndDate);
+}
+const [data] = await Promise.all([getWorkPackages()]);
+let list = document.getElementById("workPackageList");
+data.forEach(function (item) {
+    console.log(item);
+    let listIndex = document.createElement("li");
+    listIndex.innerText = item.subject + " " + item.progress;
+    list.appendChild(listIndex);
+});
+export {};
+//# sourceMappingURL=index.js.map
