ec0e71992c9961848476cad6e348e883426d4e82
Rob Tuck <rctuck@princeton.edu>
2024-11-13 16:51:31 -0500

    Added support to combine seperate json files for each wbs area to overcome OpenProject limitiation

--- js/config.js
+++ js/config.js
@@ -1,1 +1,8 @@
-export const jsonUrl = "https://uscms-software-and-computing.github.io/openproject-data/data/data.json";
+const baseUrl = "https://uscms-software-and-computing.github.io/openproject-data/data";
+export const jsonUrl = baseUrl + "/data.json";
+export const scJsonUrl = baseUrl + "/s-c.json";
+
+export const wbs1JsonUrl = baseUrl + "/wbs1.json";
+export const wbs2JsonUrl = baseUrl + "/wbs2.json";
+export const wbs3JsonUrl = baseUrl + "/wbs3.json";
+export const wbs4JsonUrl = baseUrl + "/wbs4.json";
